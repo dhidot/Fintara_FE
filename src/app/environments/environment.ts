a99305d0a2efe9e2afc826d9b7a4c4f22b4149ba
Didot <chandranindhito@gmail.com>
2025-05-27 16:01:21 +0700

    url

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
-  production: false,
-  userBaseURL: 'http://34.55.162.194/api/v1/users', // Sesuaikan dengan URL API user
-  authBaseURL: 'http://34.55.162.194/api/v1/auth',  // Sesuaikan dengan URL API backend
-  rolesBaseURL: 'http://34.55.162.194/api/v1/roles', // Sesuaikan dengan URL API roles
-  roleFeatureBaseURL: 'http://34.55.162.194/api/v1/role-features', // Sesuaikan dengan URL API roles-feature
-  plafondsBaseURL: 'http://34.55.162.194/api/v1/plafonds', // Sesuaikan dengan URL API plafond
-  pegawaiProfileBaseURL: 'http://34.55.162.194/api/v1/pegawaiprofile', // Sesuaikan dengan URL API pegawai-profile
-  pegawaiBaseURL: 'http://34.55.162.194/api/v1/pegawai', // Sesuaikan dengan URL API pegawai
-  loanRequestBaseURL: 'http://34.55.162.194/api/v1/loan-requests', // Sesuaikan dengan URL API loan-request
-  loanApprovalsBaseURL: 'http://34.55.162.194/api/v1/loan-approvals', // Sesuaikan dengan URL API loan-approval
-  featuresBaseURL: 'http://34.55.162.194/api/v1/features', // Sesuaikan dengan URL API features
-  dashboardBaseURL: 'http://34.55.162.194/api/v1/dashboard', // Sesuaikan dengan URL API dashboard
-  profileCustomerBaseURL: 'http://34.55.162.194/api/v1/profile-customer', // Sesuaikan dengan URL API profile-customer
-  customerBaseURL: 'http://34.55.162.194/api/v1/customer', // Sesuaikan dengan URL API customer
-  branchesBaseURL: 'http://34.55.162.194/api/v1/branches', // Sesuaikan dengan URL API branches
+  production: true,
+  userBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/users', // Sesuaikan dengan URL API user
+  authBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/auth',  // Sesuaikan dengan URL API backend
+  rolesBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/roles', // Sesuaikan dengan URL API roles
+  roleFeatureBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/role-features', // Sesuaikan dengan URL API roles-feature
+  plafondsBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/plafonds', // Sesuaikan dengan URL API plafond
+  pegawaiProfileBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/pegawaiprofile', // Sesuaikan dengan URL API pegawai-profile
+  pegawaiBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/pegawai', // Sesuaikan dengan URL API pegawai
+  loanRequestBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/loan-requests', // Sesuaikan dengan URL API loan-request
+  loanApprovalsBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/loan-approvals', // Sesuaikan dengan URL API loan-approval
+  featuresBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/features', // Sesuaikan dengan URL API features
+  dashboardBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/dashboard', // Sesuaikan dengan URL API dashboard
+  profileCustomerBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/profile-customer', // Sesuaikan dengan URL API profile-customer
+  customerBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/customer', // Sesuaikan dengan URL API customer
+  branchesBaseURL: 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1/branches', // Sesuaikan dengan URL API branches
 };

3c10d1e738224ba85eb957477ca11a6e506ff37f
Didot <chandranindhito@gmail.com>
2025-05-27 13:47:10 +0700

    updating package

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,17 +1,19 @@
+const BASE_URL = 'https://8b4c-103-165-222-114.ngrok-free.app/api/v1';
+
 export const environment = {
   production: false,
-  userBaseURL: 'http://localhost:8080/api/v1/users', // Sesuaikan dengan URL API user
-  authBaseURL: 'http://localhost:8080/api/v1/auth',  // Sesuaikan dengan URL API backend
-  rolesBaseURL: 'http://localhost:8080/api/v1/roles', // Sesuaikan dengan URL API roles
-  roleFeatureBaseURL: 'http://localhost:8080/api/v1/role-features', // Sesuaikan dengan URL API roles-feature
-  plafondsBaseURL: 'http://localhost:8080/api/v1/plafonds', // Sesuaikan dengan URL API plafond
-  pegawaiProfileBaseURL: 'http://localhost:8080/api/v1/pegawaiprofile', // Sesuaikan dengan URL API pegawai-profile
-  pegawaiBaseURL: 'http://localhost:8080/api/v1/pegawai', // Sesuaikan dengan URL API pegawai
-  loanRequestBaseURL: 'http://localhost:8080/api/v1/loan-requests', // Sesuaikan dengan URL API loan-request
-  loanApprovalsBaseURL: 'http://localhost:8080/api/v1/loan-approvals', // Sesuaikan dengan URL API loan-approval
-  featuresBaseURL: 'http://localhost:8080/api/v1/features', // Sesuaikan dengan URL API features
-  dashboardBaseURL: 'http://localhost:8080/api/v1/dashboard', // Sesuaikan dengan URL API dashboard
-  profileCustomerBaseURL: 'http://localhost:8080/api/v1/profile-customer', // Sesuaikan dengan URL API profile-customer
-  customerBaseURL: 'http://localhost:8080/api/v1/customer', // Sesuaikan dengan URL API customer
-  branchesBaseURL: 'http://localhost:8080/api/v1/branches', // Sesuaikan dengan URL API branches
+  userBaseURL: `${BASE_URL}/users`,
+  authBaseURL: `${BASE_URL}/auth`,
+  rolesBaseURL: `${BASE_URL}/roles`,
+  roleFeatureBaseURL: `${BASE_URL}/role-features`,
+  plafondsBaseURL: `${BASE_URL}/plafonds`,
+  pegawaiProfileBaseURL: `${BASE_URL}/pegawaiprofile`,
+  pegawaiBaseURL: `${BASE_URL}/pegawai`,
+  loanRequestBaseURL: `${BASE_URL}/loan-requests`,
+  loanApprovalsBaseURL: `${BASE_URL}/loan-approvals`,
+  featuresBaseURL: `${BASE_URL}/features`,
+  dashboardBaseURL: `${BASE_URL}/dashboard`,
+  profileCustomerBaseURL: `${BASE_URL}/profile-customer`,
+  customerBaseURL: `${BASE_URL}/customer`,
+  branchesBaseURL: `${BASE_URL}/branches`,
 };

d94ed49e3c81f7b0b66bd19f2e1d11d6dcff4d3c
Didot <chandranindhito@gmail.com>
2025-05-27 13:11:34 +0700

    updating package

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from '../src/app/app.component'; // pastikan ini benar
+import { AppComponent } from '../src/app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient, withInterceptors  } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideToastr } from 'ngx-toastr';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { provideAnimations, BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppHttpInterceptor } from './app/core/interceptors/interceptor.service'; // pastikan ini benar
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppHttpInterceptor } from './app/core/interceptors/interceptor.service';
 // ngModal
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -17,9 +17,9 @@ bootstrapApplication(AppComponent, {
     NgbModal,
     provideRouter(routes),
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    provideAnimations(),
+    provideAnimations(), // pilih ini
     provideHttpClient(withInterceptors([AppHttpInterceptor])),
-    importProvidersFrom(FormsModule, BrowserAnimationsModule),
+    importProvidersFrom(FormsModule),
     provideToastr(),
-  ]
+  ],
 });
